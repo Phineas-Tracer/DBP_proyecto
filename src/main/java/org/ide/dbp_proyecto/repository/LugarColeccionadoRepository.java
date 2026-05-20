839c85bdbe2b0fb32685ca20c9f1cbeae45a04d8
onenesz <joserodrigovaldiviezoortiz@gmail.com>
2026-05-19 19:06:32 -0500

    Entidad LugarColeccionado y su repositorio

---
+++ src/main/java/org/ide/dbp_proyecto/repository/LugarColeccionadoRepository.java
@@ -0,0 +1,8 @@
+package org.ide.dbp_proyecto.repository;
+
+import org.ide.dbp_proyecto.entity.LugarColeccionado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LugarColeccionadoRepository extends JpaRepository<LugarColeccionado, Long> {
+
+}
